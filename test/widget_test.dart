98b111e380a4c727e551c1026bcc0a4e56c77e1d
Pedro Leite <pedropedroleiteleite@gmail.com>
2021-08-30 16:58:23 -0300

    Config Import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:find_a_book/core/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:find_a_book/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
