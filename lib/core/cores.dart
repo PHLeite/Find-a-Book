23542c656acbad47ac4fba2d4389f8d8ac292529
Leticia Bianca <leticiabiancaoliveira@gmail.com>
2021-08-31 10:27:39 -0300

    Página de Login

--- lib/core/cores.dart
+++ lib/core/cores.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Cores{
   static final Color verdeAgua = Color(0XFF36BC9B);
   static final Color roxo = Color(0XFF791E94);
   static final Color azul = Color(0XFF407899);
   static final Color laranja = Color(0XFFDE6449);
+  static final Color cinza = Color(0XFF949191);
 }
