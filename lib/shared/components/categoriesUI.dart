69024449013846aa38df0dcbfdbb9a2d4429a37f
Leticia Bianca <leticiabiancaoliveira@gmail.com>
2022-01-27 14:08:21 -0300

    tentei fazer o trem de filtrar mudei oq ta comemtado no perfil so pra mostrar o dede

--- lib/shared/components/categoriesUI.dart
+++ lib/shared/components/categoriesUI.dart
@@ -1,14 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:find_a_book/core/cores.dart';
 import 'package:flutter/material.dart';
 
-class Categorie extends StatelessWidget {
-  final String Categoria;
-  const Categorie({ Key? key, required this.Categoria }) : super(key: key);
+class Categorie extends StatefulWidget {
+  final String categoria;
+  const Categorie({ Key? key , required this.categoria}) : super(key: key);
 
+  @override
+  _CategorieState createState() => _CategorieState();
+}
+
+class _CategorieState extends State<Categorie> {
+  CollectionReference books = FirebaseFirestore.instance.collection('books');
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        var snapshots = FirebaseFirestore.instance.collection('books')
+        .where('categoria', isEqualTo: widget.categoria).snapshots();
+        print('snap: $snapshots');
+      },
       child: Container(
         width: MediaQuery.of(context).size.width/3.8,
         alignment: Alignment.center,
@@ -22,7 +33,7 @@ class Categorie extends StatelessWidget {
           
           child: Align(
             alignment: Alignment.center,
-            child: Text(Categoria,
+            child: Text(widget.categoria,
             style: TextStyle(
               color: Colors.white,
               fontSize: 15,
@@ -33,5 +44,6 @@ class Categorie extends StatelessWidget {
         ),
       ),
     );  
+  
   }
 }
