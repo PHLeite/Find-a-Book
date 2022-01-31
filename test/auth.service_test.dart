8e41cd339ad26e9afa81968ad24fef06e8b1c64e
Leticia Bianca <leticiabiancaoliveira@gmail.com>
2022-01-31 17:37:56 -0300

    teste -> não mandar pra master

---
+++ test/auth.service_test.dart
@@ -0,0 +1,16 @@
+
+import 'package:find_a_book/pages/login/auth_check.dart'; 
+import 'package:flutter_test/flutter_test.dart';
+void main () {
+  group ('Testing Cart class', () { 
+    var auth = AuthCheck(); 
+    test ('Um novo usuario deve ser registrado', () {
+       var email = 'bianca@fab.com';
+       var senha = '12345678';
+       
+    }); 
+    test ('Um produto deve ser removido', () { 
+      
+    }); 
+  });
+}
